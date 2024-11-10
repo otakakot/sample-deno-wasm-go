b2e7092ae601b21f55864312b487675015c1cbd6
otakakot <kotaro.otaka@gmail.com>
2024-11-11 07:23:46 +0900

    first commit

--- index.ts
+++ index.ts
@@ -4,13 +4,21 @@ import "./wasm_exec.js";
 
 import mainwasm from "./mainwasm.ts";
 
-import { decode } from "https://deno.land/std@0.139.0/encoding/base64.ts";
+import { decodeBase64 } from "https://deno.land/std@0.224.0/encoding/base64.ts";
+
+const module = decodeBase64(mainwasm);
 
 const go = new Go();
 
-const module = decode(mainwasm);
-
+// ref: https://developer.mozilla.org/ja/docs/WebAssembly/JavaScript_interface/instantiate_static
 const { instance } = await WebAssembly.instantiate(module, go.importObject);
+
+// 第一オーバーロード
+// ... wasm バイナリーコード ( ArrayBuffer ) 形式
+
+// 第二オーバーロード
+// ... instance で import するためのオブジェクト(?)
+// ここで go.importObject を渡しているのは syscall/js を使うため
 
 go.run(instance);
 
